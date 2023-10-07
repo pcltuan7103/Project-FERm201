4602a486d9b400f1be746771f8077774cca66cbe
tuanpcl7103 <tuanpcl7103.gmail.com>
2023-10-07 10:47:20 +0700

    First Project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    $('#loginModal').modal('toggle');
+});
+document.getElementById('registerButton').addEventListener('click', function() {
+    $('#registerModal').modal('toggle');
+});
+document.getElementById('stadiumButton').addEventListener('click', function() {
+    $('#stadiumModal').modal('toggle');
+});
